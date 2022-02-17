80a6adc5fdee0b7bd3fa9b7f4b339a6884cda713
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-02-18 01:35:02 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from 'components/theme';
 import { AccentGlobal } from 'components/theme/Accent';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
