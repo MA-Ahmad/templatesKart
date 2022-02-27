7afa4860e0ee7c1f6a307fde19c5b9ed2dbf77db
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-02-27 22:56:10 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.child_process = false;
+    }
+    return config;
+  }
+};
