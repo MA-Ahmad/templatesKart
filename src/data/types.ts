aa9e1d6bb0e3306d7d702cd48427905d50f37272
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-03-02 14:35:51 +0500

    Create types.ts

---
+++ src/data/types.ts
@@ -0,0 +1,30 @@
+import { IconType } from 'react-icons';
+
+export interface Project {
+  name: string;
+  id: string;
+  description: string;
+  githubRepoName: string;
+  imageUrl: string;
+  liveLink: string;
+  techStack: string[];
+  pages: Page[];
+  components: Component[];
+}
+
+export interface Page {
+  name: string;
+  id: string;
+  icon: IconType;
+  description?: string;
+  components?: Component[];
+}
+
+export interface Component {
+  name: string;
+  filename: string;
+  type: string;
+  page_id: string;
+  tags?: string[];
+  codeLink?: string;
+}
