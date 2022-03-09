a2f6a38779827f2d8104971b5af72d001bd61a10
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-03-10 04:25:42 +0500

    Create constants.ts

---
+++ src/data/constants.ts
@@ -0,0 +1,6 @@
+export const GITHUB_BASE_URL = 'https://github.com/MA-Ahmad';
+export const GITHUB_API_URL = 'https://api.github.com';
+export const GITHUB_USERNAME = 'MA-Ahmad';
+export const GITHUB_REPO = 'myportfolio';
+
+export const DEVTO_API_URL = 'https://dev.to/api';
