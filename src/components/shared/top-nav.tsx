b9ae5eec394e8c37aa783434d4c037e47d93c27f
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-03-23 01:08:23 +0500

    Update top-nav.tsx

--- src/components/shared/top-nav.tsx
+++ src/components/shared/top-nav.tsx
@@ -1,10 +1,24 @@
-import { Box, Flex, HStack, useColorModeValue, Heading, Container } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  HStack,
+  useColorModeValue,
+  Heading,
+  Container,
+  Image,
+  Icon,
+  Text
+} from '@chakra-ui/react';
 import { ColorModeSwitcher } from '../theme/ColorModeSwitcher';
 import NextLink from 'next/link';
 import { AccentPicker } from 'components/theme/Accent';
 import DropDownMenu from './menu-list';
+import { GoChevronLeft, GoChevronRight } from 'react-icons/go';
+import { useLinkColor, useHoverLinkColor } from 'components/theme';
 
 export default function TopNav() {
+  const linkColor = useLinkColor();
+
   return (
     <>
       <Box
@@ -20,12 +34,35 @@ export default function TopNav() {
         zIndex="55"
         top="0"
       >
-        <Container maxW={'1280px'}>
+        <Container maxW={'1280px'} p={{ base: 0, md: 'inherit' }}>
           <Flex h={16} alignItems={'center'} justifyContent={'space-between'} mx="auto">
-            <HStack spacing={8} alignItems={'center'}>
+            <HStack spacing={{ base: 0, sm: 3 }} alignItems={'center'}>
               <NextLink href={'/'} passHref>
-                <Heading as="h1" fontSize={['xl', '2xl']} cursor="pointer">
-                  TemplatesKart
+                <Heading as="h1" fontSize={['lg', 'md', 'xl', '3xl']} cursor="pointer">
+                  <Flex position="relative">
+                    <Icon
+                      position="relative"
+                      as={GoChevronLeft}
+                      transform={'rotate(-15deg)'}
+                      w={7}
+                      h={7}
+                      color={linkColor}
+                      top={1}
+                    />
+                    <HStack d={{ base: 'none', sm: 'flex' }} spacing={2}>
+                      <Text textShadow="1px 2px #179c40">Templates</Text>
+                      <Text textShadow="1px 2px #179c40">Kart</Text>
+                    </HStack>
+                    <Icon
+                      position="relative"
+                      as={GoChevronRight}
+                      transform={'rotate(-15deg)'}
+                      w={7}
+                      h={7}
+                      color={linkColor}
+                      bottom={1}
+                    />
+                  </Flex>
                 </Heading>
               </NextLink>
               <DropDownMenu />
