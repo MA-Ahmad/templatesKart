d23ed21937fa65a6a9dc859f5eb203812d9cd1b6
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-03-25 02:55:25 +0500

    Add HeadingSection component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,15 @@
-import { Box, Heading, Text, SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react';
 import RootLayout from 'components/layouts/rootLayout';
-import { useLinkColor } from 'components/theme';
-import UnderlinedText from 'components/shared/underlined-text';
+import HeadingSection from 'components/shared/heading-section';
 import { projects } from 'data/projects';
 import Card from 'components/home/Card';
 import { SEO } from 'components/SEO';
 
 export default function Index() {
-  const linkColor = useLinkColor();
-
   return (
     <RootLayout>
       <SEO />
-      <Box textAlign="center">
-        <Heading as="h1" fontSize={{ base: '3xl', md: '4xl' }} mb={5}>
-          <UnderlinedText color={linkColor}>TemplatesKart</UnderlinedText>
-        </Heading>
-        <Text fontSize="lg" fontWeight="semibold" mt={2}>
-          Beautiful and responsive React components to build your application
-        </Text>
-        <Text fontSize="lg" fontWeight="semibold" mt={2}>
-          Copy code from live websites
-        </Text>
-      </Box>
+      <HeadingSection title="Projects" />
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={5} mt={12} mb={4}>
         {projects.map((project, index) => (
           <Card key={index} project={project} />
