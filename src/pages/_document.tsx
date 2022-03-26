9093d6f593e2be2230bb292bc2c0ecd19d4f769f
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-03-27 01:03:36 +0500

    Add a empty line

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.png" />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
