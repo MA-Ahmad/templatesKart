24a7e0d1779a414acc09e9486ce9f9891bb267cd
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-04-16 02:58:28 +0500

    Modify menu button styling

--- src/preview/projects/blog-app/pages/home/header.jsx
+++ src/preview/projects/blog-app/pages/home/header.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Box, Heading, Flex, Text, Button, Link } from '@chakra-ui/react';
+import { Box, Heading, Flex, Button, Link, useColorModeValue } from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
 
-// Note: This code could be better, so I'd recommend you to understand how I solved and you could write yours better :)
-const Header = (props) => {
+const Header = () => {
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow(!show);
 
@@ -27,7 +26,14 @@ const Header = (props) => {
           </Heading>
         </Flex>
 
-        <Box display={['block', 'none', 'none', 'none']} onClick={handleToggle}>
+        <Box
+          display={['block', 'none', 'none', 'none']}
+          onClick={handleToggle}
+          cursor="pointer"
+          p={3}
+          bg={useColorModeValue('gray.300', 'gray.600')}
+          rounded="md"
+        >
           <svg fill="white" width="12px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
