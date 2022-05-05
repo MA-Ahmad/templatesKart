5c0924c159a3aafee1babd9c49e4735ec2a23426
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-05-06 00:28:42 +0500

    Update with-logo-left.tsx

--- src/preview/components/footers/with-logo-left.tsx
+++ src/preview/components/footers/with-logo-left.tsx
@@ -1,6 +1,6 @@
-import { Stack, HStack, Link, Divider, Image, IconButton } from '@chakra-ui/react';
+import { Stack, HStack, Link, Divider, Image, IconButton, LinkProps } from '@chakra-ui/react';
 // Here we have used react-icons package for the icons
-import { FaGithub, FaDev, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const links = ['Blog', 'Documentation', 'Careers', 'Sign up', 'Terms of use', 'Privacy policy'];
 const accounts = [
@@ -27,52 +27,40 @@ const accounts = [
 const Footer = () => {
   return (
     <Stack
+      maxW="5xl"
+      marginInline="auto"
       p={8}
       spacing={{ base: 8, md: 0 }}
       justifyContent="space-between"
       alignItems="center"
       direction={{ base: 'column', md: 'row' }}
     >
-      <Image w="100px" src="/assets/images/layouts/hero_image.png" alt="TemplatesKart" />
+      <Link href="https://templateskart.com" isExternal>
+        <Image w="100px" src="/assets/images/layouts/hero_image.png" alt="TemplatesKart" />
+      </Link>
 
       {/* Desktop Screen */}
       <HStack spacing={4} alignItems="center" d={{ base: 'none', md: 'flex' }}>
         {links.map((link, index) => (
-          <Link key={index} href="#" fontSize="md" _hover={{ textDecoration: 'underline' }}>
-            {link}
-          </Link>
+          <CustomLink key={index}>{link}</CustomLink>
         ))}
       </HStack>
 
       {/* Mobile and Tablet Screens */}
       <Stack d={{ base: 'flex', md: 'none' }} alignItems="center">
         <HStack alignItems="center">
-          <Link href="#" fontSize="md" _hover={{ textDecoration: 'underline' }}>
-            Sign up
-          </Link>
+          <CustomLink>Sign up</CustomLink>
           <Divider h="1rem" orientation="vertical" />
-          <Link href="#" fontSize="md" _hover={{ textDecoration: 'underline' }}>
-            Blog
-          </Link>
+          <CustomLink>Blog</CustomLink>
           <Divider h="1rem" orientation="vertical" />
-          <Link href="#" fontSize="md" _hover={{ textDecoration: 'underline' }}>
-            Career
-          </Link>
+          <CustomLink>Career</CustomLink>
         </HStack>
         <HStack alignItems="center">
-          <Link href="#" fontSize="md" _hover={{ textDecoration: 'underline' }}>
-            Documentation
-          </Link>
+          <CustomLink>Documentation</CustomLink>
           <Divider h="1rem" orientation="vertical" />
-          <Link href="#" fontSize="md" _hover={{ textDecoration: 'underline' }}>
-            Terms of use
-          </Link>
+          <CustomLink>Terms of use</CustomLink>
         </HStack>
-        <HStack alignItems="center">
-          <Link href="#" fontSize="md" _hover={{ textDecoration: 'underline' }}>
-            Privacy policy
-          </Link>
-        </HStack>
+        <CustomLink>Privacy policy</CustomLink>
       </Stack>
 
       <Stack direction="row" spacing={5} pt={{ base: 4, md: 0 }} alignItems="center">
@@ -90,6 +78,14 @@ const Footer = () => {
         ))}
       </Stack>
     </Stack>
+  );
+};
+
+const CustomLink = ({ children, ...props }: LinkProps) => {
+  return (
+    <Link href="#" fontSize="sm" _hover={{ textDecoration: 'underline' }} {...props}>
+      {children}
+    </Link>
   );
 };
 
