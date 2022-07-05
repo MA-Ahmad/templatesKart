29f90dbb20b05ea0f1897c949217a670df98f97d
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-07-05 16:20:01 +0500

    Create separate icons file

---
+++ src/components/layouts/components-preview/icons.tsx
@@ -0,0 +1,19 @@
+export const LargeIcon = () => {
+  return (
+    <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="18" height="10" fill="#ffffff" />
+      <path d="M0 13H16" stroke="#ffffff" stroke-width="2" />
+      <path d="M0 16.5H12" stroke="#ffffff" />
+    </svg>
+  );
+};
+
+export const SmallIcon = () => {
+  return (
+    <svg width="22" height="10" viewBox="0 0 22 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="12" height="10" fill="#ffffff" />
+      <line x1="13.5" y1="2" x2="21.5" y2="2" stroke="#ffffff" stroke-width="2" />
+      <line x1="14" y1="7.5" x2="20" y2="7.5" stroke="#ffffff" />
+    </svg>
+  );
+};
