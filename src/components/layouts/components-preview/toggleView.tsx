13f21ed111ea12fc574e9ff2cb21478347655953
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-07-05 16:20:53 +0500

    Create toggleView.tsx

---
+++ src/components/layouts/components-preview/toggleView.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Stack, Tooltip, Flex, useColorModeValue } from '@chakra-ui/react';
+import { useLinkColor } from 'preview/projects/my-website/pages/theme';
+import { LargeIcon, SmallIcon } from './icons';
+
+interface ToggleViewProps {
+  viewTypeId: Number;
+  setViewTypeId: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const viewTypes = [
+  { id: 1, icon: <LargeIcon />, label: 'Large cards' },
+  { id: 2, icon: <SmallIcon />, label: 'Small cards' }
+];
+
+const ToggleView = ({ viewTypeId, setViewTypeId }: ToggleViewProps) => {
+  const linkColor = useLinkColor();
+
+  return (
+    <Stack px={{ base: 0, sm: 10 }} direction="row" w="100%" justifyContent="flex-end">
+      {viewTypes.map((type) => (
+        <Tooltip hasArrow label={type.label} placement="top">
+          <Flex
+            p={3}
+            w={10}
+            alignItems="center"
+            bg={viewTypeId === type.id ? linkColor : useColorModeValue('#a0aebf', '#4a5567')}
+            lineHeight={1}
+            rounded="md"
+            onClick={() => setViewTypeId(type.id)}
+            cursor="pointer"
+          >
+            {type.icon}
+          </Flex>
+        </Tooltip>
+      ))}
+    </Stack>
+  );
+};
+
+export default ToggleView;
