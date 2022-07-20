b8e0a3685558c5b2d6c28249e1145483d1502419
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-07-20 16:51:07 +0500

    Modify scrollToTop button

--- src/components/shared/scrollToTop.tsx
+++ src/components/shared/scrollToTop.tsx
@@ -1,35 +1,57 @@
-import { IconButton, Box } from '@chakra-ui/react';
+import { Flex, Box, Icon, Tooltip, useColorModeValue } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { FiArrowUp } from 'react-icons/fi';
 import { useLinkColor } from 'components/theme';
+import { useLocalSetting } from '../theme/hooks/useLocalSetting';
 
 const valuePageYOffset = 300;
 
 const ScrollToTop = () => {
-  const linkColor = useLinkColor();
+  const [isRightDirection, setIsRightDirection] = useLocalSetting('isRightDirection', true);
   const scrollToTop = () => window.scroll({ top: 0, behavior: 'smooth' });
   const getVisible = () =>
     document.body.scrollTop > valuePageYOffset ||
     document.documentElement.scrollTop > valuePageYOffset;
   const [isVisible, setIsVisible] = useState(false);
+
   useEffect(() => {
     setIsVisible(() => getVisible());
     window.onscroll = () => setIsVisible(() => getVisible());
   }, []);
 
   return (
-    <Box pos="fixed" bottom="30" right="4" visibility={isVisible ? 'visible' : 'hidden'}>
-      <IconButton
-        aria-label="Back to top"
-        title="Back to top"
-        icon={<FiArrowUp />}
-        rounded="full"
-        bg={linkColor}
-        color="white"
-        colorScheme="brand"
-        onClick={scrollToTop}
-      />
-    </Box>
+    <Tooltip label="Scroll to top" placement="left" aria-label="Scroll to top">
+      <Box
+        pos="fixed"
+        transition="all 0.1s ease-in-out"
+        bottom={isRightDirection ? '20' : '5'}
+        right="5"
+        visibility={isVisible ? 'visible' : 'hidden'}
+        zIndex={5}
+      >
+        <Flex
+          rounded="full"
+          bg={'var(--colors-accent-200)'}
+          p="0.1rem"
+          boxShadow={useColorModeValue(
+            '2px 6px 8px 2px var(--colors-accent-100)',
+            '2px 6px 8px rgba(9, 17, 28, 0.9)'
+          )}
+          cursor="pointer"
+          onClick={scrollToTop}
+        >
+          <Flex
+            rounded="full"
+            bg={useColorModeValue('gray.100', 'gray.700')}
+            p={3}
+            alignItems="center"
+            _hover={{ bg: useColorModeValue('gray.100', 'gray.600') }}
+          >
+            <Icon as={FiArrowUp} color={useColorModeValue('gray.900', 'white')} w={5} h={5} />
+          </Flex>
+        </Flex>
+      </Box>
+    </Tooltip>
   );
 };
 
